fbf9637f5f71a238943ceb8e0fa3a00fbb2b82a7
curus-m <eliminationatonce@gmail.com>
2021-06-29 00:52:15 +0900

    implement chartjs

--- src/Thermometer.tsx
+++ src/Thermometer.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import './Thermometer.scss';
-
+import { Bar } from 'react-chartjs-2';
 class Thermometer extends React.Component {
-    
     constructor(props : any) {
         super(props);
         this.state = {
@@ -11,8 +10,6 @@ class Thermometer extends React.Component {
     }
     componentDidMount() {
         const url : string = "http://192.168.0.39:3000/temperature";
-          // "http://localhost:3000/temperature"
-        // "http://192.168.0.39:3000/getTemp";
         fetch(url, {
           method: 'POST',
           headers: {
@@ -41,14 +38,28 @@ class Thermometer extends React.Component {
     render(){
         const { tempDatas } : any= this.state;
         const tempDataList = tempDatas.map((tempData : any) => 
-            <div> 
+            <div key="{tempData.time}"> 
                 {tempData.time} / {tempData.temperature}℃ / {tempData.humidity}%
             </div>
         );
+        const graphData : Object = {
+          labels : ['3','6','9','12','15','18','21','24','27','30'],
+          datasets: [
+            // 表示するデータセット
+            {
+              data: tempDatas,
+              label: '温度'
+            },
+          ]
+        }
         return (
             <div>
-                {tempDataList}
+                {/* {tempDataList} */}
+                <div>
+                  <Bar data={graphData} type="Bar" />
+                </div>
             </div>
+            
         )
         
     }
