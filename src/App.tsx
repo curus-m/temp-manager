85375906a9a296fa7883b2774c313c724f29c3e7
curus-m <eliminationatonce@gmail.com>
2021-07-06 23:12:22 +0900

    navbar improved

--- src/App.tsx
+++ src/App.tsx
@@ -7,46 +7,64 @@ import {
   Route, Link
 } from "react-router-dom";
 import './App.scss';
+class App extends React.Component<{}, any> { 
+    constructor(props : any) {
+      super(props);
+      this.state = {
+        isExpanded: false
+      };
+    }
+  
+  handleToggle(e : any) {
+    e.preventDefault();
+    console.log("click!")
+    this.setState({
+      isExpanded: !this.state.isExpanded
+    });
+  }
+  render() {
+    const { isExpanded } = this.state;
 
-function App() {
-  // function myFunction() {
-  //   this.class 
-  // }
-  return (
-    <div className="App">
-      <Router>
-        <div>
-          <nav className="navbar navbar-light bg-success">
-            <ul className="nav nav-pills">
-              <li className="nav-items">
-                <Link to ="/" className="nav-link">Home</Link>
-              </li>
-              <li className="nav-items">
-                <Link to ="/thermometer" className="nav-link">Thermometer</Link>
+    return (
+      <div className="App">
+        <Router>
+          <div className="topnav">
+            <nav className="navbar navbar-light bg-success">
+              <ul className="nav">
+                <li className={`nav-items ${isExpanded ? "responsive" : ""}`}>
+                  <Link to ="/" className="nav-link">Home</Link>
                 </li>
-              <li className="nav-items" >
-                <Link to="/about" className="nav-link">About</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <Switch>
-          <Route path="/thermometer">
-                <Thermometer/>
-              </Route>
-              <Route path="/about">
-                <About />
-              </Route>
-              <Route path="/">
-                <Mainpage/>
-              </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
-}
-function About() {
-  return <h2>About</h2>;
-}
+                <li className={`nav-items ${isExpanded ? "responsive" : ""}`}>
+                  <Link to ="/thermometer" className="nav-link">Thermometer</Link>
+                </li>
+                <li className={`nav-items ${isExpanded ? "responsive" : ""}`}>
+                  <Link to="/about" className="nav-link">About</Link>
+                </li>
+                <li className="icon"  onClick={e => this.handleToggle(e)}>
+                  <i className="fa fa-bars"></i>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <Switch>
+            <Route path="/thermometer">
+              <Thermometer/>
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Mainpage/>
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
 
+    function About() {
+      return <h2>About</h2>;
+    }
+  }
+
+}
 export default App;
